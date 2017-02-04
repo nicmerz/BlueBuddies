bee32c85a6912c461dfb2079509c97a3b1b7ad0d
bichengliu <bicheng_liu@hotmail.com>
2017-02-04 11:11:11 -0500

    updated the general binning of clients procedure

--- testingPYfiles/general.py
+++ testingPYfiles/general.py
@@ -12,12 +12,12 @@ def main():
         ,'anxiety':{'something_emotions':{}},
                             'eating':{'energetic':0,'weight':0,'social_image':{}}}} # not to be implemented but kept as a key for documentation
     myinput = input('How are you feeling right now? Tell me all the details! \n') # can use hq sentiment
-    out1 = indicoio.sentiment_hq(myinput) # element 1 is first hq
-    client = [out1]
+    dep = indicoio.sentiment_hq(myinput) # element 1 is first hq
+    client = [dep]
     out1feelings = indicoio.emotion(myinput)
     client += [out1feelings] # we also keep the feelings
-
-    if(out1 > 0.85):
+    print(dep)
+    if(dep > 0.85):
         myinput = input('How are you really feeling? This is a safe environment! \n') #element 2 is either 2 (illegal from indicoio), or second hq response
         client += [indicoio.emotion(myinput)]
     else:
@@ -25,8 +25,25 @@ def main():
         client += [{}]
 
     myinput = input('Do you feel anxious about /something/ at the moment?')
-    client += [indicoio.sentiment_hq(myinput)] # a sufficiently high number will trigger the anxiety scripts but keep this value for later
-
+    anxious =indicoio.sentiment_hq(myinput)
+    client += [anxious] # a sufficiently high number will trigger the anxiety scripts but keep this value for later
+    print(anxious)
     myinput = input('How do you feel about having a nice meal? \n')
-    client += [indicoio.sentiment_hq(myinput)]
+    meal = indicoio.sentiment_hq(myinput)
+    client += [meal]
+    print(meal)
+# now we have to do decision-making based on testing. F U C K Y E A H L E T S G O O O
+    if((dep < 0.75) & (anxious > 0.2) & (meal > 0.2)):
+        outp = 1 #1 corresponds to almost exclusively depressed
+    elif((dep > 0.2) & (anxious < 0.75) & (meal > 0.2)):
+        outp = 2 #2 corresponds to a strong probability of an exclusive anxiety problem
+    elif((dep > 0.2) & (anxious > 0.2) & (meal < 0.75)):
+        outp = 3 #3 corresponds to a strong probability of exclusive bulemia/anorexia
+    elif((dep < anxious) & (dep < meal)):
+        outp = 4 #4 corresponds to depression being the dominant but not exclusive problem
+    elif((anxious < dep) & (anxious < meal)):
+        outp = 5 #5 corresponds to anxiety being the dominant but not exclusive problem
+    elif((meal < dep) & (meal < anxious)):
+        outp = 6 #6 corresponds to bulemia/anorexia being a dominant but not exclusive problem
+    print(outp)
 if __name__ == "__main__": main()
